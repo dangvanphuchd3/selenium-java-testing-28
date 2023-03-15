edb0f31a95e5bae992a2afb7eee8e7ec487fa6a0
dangvanphuc <dangvanphuchd3@gmail.com>
2023-03-15 23:59:36 +0700

    Action Part I

--- selenium/webdriver/Topic_13_Action_Part_I.java
+++ selenium/webdriver/Topic_13_Action_Part_I.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -32,19 +33,48 @@ public class Topic_13_Action_Part_I {
 		
 	}
 
-	@Test
-	public void TC_01_() {
+	public void TC_01_Tooltip() {
+		driver.get("https://automationfc.github.io/jquery-tooltip/");
+		
+		action.moveToElement(driver.findElement(By.cssSelector("input#age"))).perform();
+		sleepInSecond(3);
+		
+		System.out.println(driver.findElement(By.cssSelector("div.ui-tooltip-content")).getText());
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("div.ui-tooltip-content")).getText(), "We ask for your age only for statistical purposes.");
+		
+	}
+
+	public void TC_02_Myntra() {
+		driver.get("https://www.myntra.com/");
+		
+		action.moveToElement(driver.findElement(By.xpath("//div[@class='desktop-navLink']//a[text()='Kids']"))).perform();
+		sleepInSecond(3);
+		
+		driver.findElement(By.xpath("//div[@class='desktop-navLink']//a[text()='Home & Bath']")).click();
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("span.breadcrumbs-crumb")).getText(), "Kids Home Bath");
 		
 	}
 
 	@Test
-	public void TC_02_() {
+	public void TC_03_Fahasa() {
+		driver.get("https://www.fahasa.com/");
+		sleepInSecond(10);
 		
-	}
+		//Hover lần 1
+		action.moveToElement(driver.findElement(By.cssSelector("span.icon_menu"))).perform();
+		sleepInSecond(3);
+		
+		//Hover lần 2
+		action.moveToElement(driver.findElement(By.xpath("//a[@title='Đồ Chơi']"))).perform();
+		sleepInSecond(3);
+		
+		//Click
+		driver.findElement(By.xpath("//div[contains(@class,'fhs_menu_content')]//a[text()='Ô Tô']")).click();
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//ol[@class='breadcrumb']//strong[text()='Ô Tô - Xe Buýt']")).isDisplayed());
 
-	@Test
-	public void TC_03_() {
-		
 	}
 	
 	public void sleepInSecond(long timeInSecond) {
