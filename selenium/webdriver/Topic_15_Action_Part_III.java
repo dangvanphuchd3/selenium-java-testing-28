b2ca99a3ee77bc0f257ef1acea3a560407254433
user <user@PhucDV>
2023-03-17 12:42:52 +0700

    Topic_15_Action_Part III lần 2

--- selenium/webdriver/Topic_15_Action_Part_III.java
+++ selenium/webdriver/Topic_15_Action_Part_III.java
@@ -5,8 +5,10 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -33,8 +35,8 @@ public class Topic_15_Action_Part_III {
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 	}
-
-	@Test
+	
+	//@Test
 	public void TC_01_Double_Click() {
 		driver.get("https://automationfc.github.io/basic-form/index.html");
 		
@@ -48,13 +50,50 @@ public class Topic_15_Action_Part_III {
 		Assert.assertEquals(driver.findElement(By.cssSelector("p#demo")).getText(), "Hello Automation Guys!");
 	}
 
-	@Test
-	public void TC_02_() {
+	//@Test
+	public void TC_02_Right_Click() {
+		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
 		
+		action.contextClick(driver.findElement(By.cssSelector("span.context-menu-one"))).perform();
+		sleepInSecond(3);
+		
+		Assert.assertTrue(driver.findElement(By.cssSelector("li.context-menu-icon-quit")).isDisplayed());
+		
+		action.moveToElement(driver.findElement(By.cssSelector("li.context-menu-icon-quit"))).perform();
+		sleepInSecond(3);
+		
+		Assert.assertTrue(driver.findElement(By.cssSelector("li.context-menu-icon-quit.context-menu-visible.context-menu-hover")).isDisplayed());
+		
+		action.click(driver.findElement(By.cssSelector("li.context-menu-icon-quit"))).perform();
+		sleepInSecond(3);
+		
+		driver.switchTo().alert().accept();
+		sleepInSecond(3);
+		
+		Assert.assertFalse(driver.findElement(By.cssSelector("li.context-menu-icon-quit")).isDisplayed());
 	}
 
-	@Test
-	public void TC_03_() {
+	//@Test
+	public void TC_03_Drag_And_Drop_HTML4() {
+		driver.get("https://automationfc.github.io/kendo-drag-drop/");
+		
+		WebElement smallCircle = driver.findElement(By.cssSelector("div#draggable"));
+		WebElement bigCircle = driver.findElement(By.cssSelector("div#droptarget"));
+		
+		action.dragAndDrop(smallCircle, bigCircle).perform();
+		sleepInSecond(3);
+		
+		// Verify Text
+		Assert.assertEquals(bigCircle.getText(), "You did great!");
+		
+		// Verify Background Color
+		String bigCircleBackGroundColor = bigCircle.getCssValue("background-color");
+		System.out.println(bigCircleBackGroundColor);
+		
+		Assert.assertEquals(Color.fromString(bigCircleBackGroundColor).asHex().toUpperCase(), "#03A9F4");
+	}
+	
+	public void TC_04_Drag_And_Drop_HTML5() {
 		
 	}
 	
