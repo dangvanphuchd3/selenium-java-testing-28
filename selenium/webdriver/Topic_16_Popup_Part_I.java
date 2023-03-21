165f145c60731b311a7c061e7400ad5a276a1853
dangvanphuc <dangvanphuchd3@gmail.com>
2023-03-21 20:50:34 +0700

    Topic_16_Popup_Part_I lan 2

--- selenium/webdriver/Topic_16_Popup_Part_I.java
+++ selenium/webdriver/Topic_16_Popup_Part_I.java
@@ -5,6 +5,8 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -24,6 +26,12 @@ public class Topic_16_Popup_Part_I {
 		}
 		
 		driver = new FirefoxDriver();
+		
+		FirefoxOptions options = new FirefoxOptions();
+		options.setProfile(new FirefoxProfile());
+		options.addPreference("dom.webnotifications.enabled", false);
+		driver = new FirefoxDriver(options);
+		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 	}
@@ -57,8 +65,30 @@ public class Topic_16_Popup_Part_I {
 	}
 
 	@Test
-	public void TC_02_() {
+	public void TC_02_Fixed_In_DOM_KyNa() {
+		driver.get("https://skills.kynaenglish.vn/");
 		
+		By loginPopup = By.cssSelector("div#k-popup-account-login");
+		
+		Assert.assertFalse(driver.findElement(loginPopup).isDisplayed());
+		
+		driver.findElement(By.cssSelector("a.login-btn")).click();
+		sleepInSecond(2);
+		
+		Assert.assertTrue(driver.findElement(loginPopup).isDisplayed());
+		
+		driver.findElement(By.cssSelector("input#user-login")).sendKeys("automation@gmail.com");
+		driver.findElement(By.cssSelector("input#user-password")).sendKeys("123456");
+		
+		driver.findElement(By.cssSelector("button#btn-submit-login")).click();
+		sleepInSecond(3);
+		
+		Assert.assertEquals(driver.findElement(By.cssSelector("div#password-form-login-message")).getText(), "Sai tên đăng nhập hoặc mật khẩu");
+		
+		driver.findElement(By.cssSelector("button.k-popup-account-close")).click();
+		sleepInSecond(2);
+		
+		Assert.assertFalse(driver.findElement(loginPopup).isDisplayed());
 	}
 
 	@Test
