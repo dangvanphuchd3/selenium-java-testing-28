591445611f99556938f12e1b7fb60895d17c915d
dangvanphuc <dangvanphuchd3@gmail.com>
2023-04-06 22:17:13 +0700

    Topic_18_Window_Tab - Nộp bài lần 1

---
+++ selenium/webdriver/Topic_18_Window_Tab.java
@@ -0,0 +1,67 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_18_Window_Tab {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac OS")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+		
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+	}
+
+	@Test
+	public void TC_01_Exercise_01() {
+		// Step 01: Truy cập vào trang parent page
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		
+		// Lấy ra id của tab hiện tại
+		String parentPageWindowID = driver.getWindowHandle();
+		System.out.println("Parent Page = " + parentPageWindowID);
+		
+		// Step 02: Click link "GOOGLE" -> Switch qua page mới
+		// Click vào link "GOOGLE"
+	}
+
+	@Test
+	public void TC_02_() {
+		
+	}
+
+	@Test
+	public void TC_03_() {
+		
+	}
+	
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+			// 1000 ms = 1s
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public void afterClass() {
+		//driver.quit();
+	}
+}
