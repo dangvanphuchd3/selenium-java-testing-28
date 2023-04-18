55b59ffbc1d1e37594de5803950365900ff107ca
user <user@PhucDV>
2023-04-18 12:49:57 +0700

    Topic_20_Upload_File Lần 3

--- selenium/webdriver/Topic_20_Upload_File.java
+++ selenium/webdriver/Topic_20_Upload_File.java
@@ -1,9 +1,12 @@
 package webdriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -14,6 +17,7 @@ public class Topic_20_Upload_File {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
+	JavascriptExecutor jsExecutor;
 	
 	String beachFileName = "beach.jpg";
 	String computerFileName = "computer.jpg";
@@ -33,24 +37,91 @@ public class Topic_20_Upload_File {
 		
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
+		jsExecutor = (JavascriptExecutor) driver;
 	}
 
-	@Test
+	//@Test
 	public void TC_01_One_File_Per_Time() {
 		driver.get("https://blueimp.github.io/jQuery-File-Upload/");
 		
-		driver.findElement(By.cssSelector("input[type='file']")).sendKeys("D:\\TestIO\\computer.jpg");
+		// Load file lên
+		driver.findElement(By.cssSelector("input[type='file']")).sendKeys(beachFilePath);
+		sleepInSecond(1);
+		driver.findElement(By.cssSelector("input[type='file']")).sendKeys(computerFilePath);
+		sleepInSecond(1);
+		driver.findElement(By.cssSelector("input[type='file']")).sendKeys(mountainFilePath);
+		sleepInSecond(1);
+		
+		// Verify file được load lên thành công
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + beachFileName + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + computerFileName + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + mountainFileName + "']")).isDisplayed());
+		
+		// Click upload
+		List<WebElement> uploadButton = driver.findElements(By.cssSelector("table button.start"));
+		for (WebElement button : uploadButton) {
+			button.click();
+			sleepInSecond(3);
+		}
+		
+		// Verify upload thành công (Link)
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + beachFileName + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + computerFileName + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + mountainFileName + "']")).isDisplayed());
+		
+		// Verify upload thành công (image)
+		Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + beachFileName + "')]"));
+		Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + computerFileName + "')]"));
+		Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + mountainFileName + "')]"));
+		
 	}
 
 	@Test
-	public void TC_02_() {
+	public void TC_02_Multiple_File_Per_Time() {
+		driver.get("https://blueimp.github.io/jQuery-File-Upload/");
+		
+		// Load file lên
+		driver.findElement(By.cssSelector("input[type='file']")).sendKeys(beachFilePath + "\n" + computerFilePath + "\n" + mountainFilePath);
+		sleepInSecond(1);
+		
+		// Verify file được load lên thành công
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + beachFileName + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + computerFileName + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='name' and text()='" + mountainFileName + "']")).isDisplayed());
+		
+		// Click upload
+		List<WebElement> uploadButton = driver.findElements(By.cssSelector("table button.start"));
+		for (WebElement button : uploadButton) {
+			button.click();
+			sleepInSecond(3);
+		}
+		
+		// Verify upload thành công (Link)
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + beachFileName + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + computerFileName + "']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='" + mountainFileName + "']")).isDisplayed());
+		
+		// Verify upload thành công (image)
+		Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + beachFileName + "')]"));
+		Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + computerFileName + "')]"));
+		Assert.assertTrue(isImageLoaded("//img[contains(@src,'" + mountainFileName + "')]"));
 		
 	}
 
 	@Test
 	public void TC_03_() {
 		
+	}
+	
+	public boolean isImageLoaded(String locator) {
+		boolean status = (boolean) jsExecutor.executeScript(
+				"return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0",
+				getElement(locator));
+		return status;
+	}
+	
+	public WebElement getElement(String locator) {
+		return driver.findElement(By.xpath(locator));
 	}
 	
 	public void sleepInSecond(long timeInSecond) {
