75b39e88722069497c4829d66efa78df86844f36
user <user@PhucDV>
2023-05-08 12:46:10 +0700

    ExplicitWait Exercise lần 1

---
+++ selenium/webdriver/Topic_23_ExplicitWait_Exercise.java
@@ -0,0 +1,71 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_23_ExplicitWait_Exercise {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+	WebDriverWait explicitWait;
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Mac OS")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		}
+		
+		driver = new FirefoxDriver();
+		
+	}
+
+	@Test
+	public void TC_01_Ajax_Loading() {
+		driver.get("https://demos.telerik.com/aspnet-ajax/ajaxloadingpanel/functionality/explicit-show-hide/defaultcs.aspx");
+		
+		explicitWait = new WebDriverWait(driver, 15);
+		
+		// Wait cho Date Picker được hiển thị
+		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.RadCalendar")));
+		
+		// Wait cho ngày 19 được phép click
+		explicitWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text='19']")));
+		
+		
+	}
+
+	@Test
+	public void TC_02_() {
+		
+	}
+
+	@Test
+	public void TC_03_() {
+		
+	}
+	
+	public void sleepInSecond(long timeInSecond) {
+		try {
+			Thread.sleep(timeInSecond * 1000);
+			// 1000 ms = 1s
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterClass
+	public void afterClass() {
+		//driver.quit();
+	}
+}
