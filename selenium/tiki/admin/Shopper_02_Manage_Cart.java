c5c1b1282842d7d8a50916d890b635c8fd2c38dd
dangvanphuc <dangvanphuchd3@gmail.com>
2023-06-04 22:53:05 +0700

    TestNG - Commit lan 1

---
+++ selenium/tiki/admin/Shopper_02_Manage_Cart.java
@@ -0,0 +1,25 @@
+package tiki.admin;
+
+import org.testng.annotations.Test;
+
+public class Shopper_02_Manage_Cart {
+  @Test
+  public void Cart_01_Create_Visa() {
+	  
+  }
+  
+  @Test
+  public void Cart_02_View_Visa() {
+	  
+  }
+  
+  @Test
+  public void Cart_03_Update_Visa() {
+	  
+  }
+  
+  @Test
+  public void Cart_04_Delete_Visa() {
+	  
+  }
+}
---
+++ selenium/tiki/user/User_02_Order.java
@@ -0,0 +1,25 @@
+package tiki.user;
+
+import org.testng.annotations.Test;
+
+public class User_02_Order {
+  @Test
+  public void Product_01_Create_Product() {
+	  
+  }
+  
+  @Test
+  public void Product_02_View_Product() {
+	  
+  }
+  
+  @Test
+  public void Product_03_Update_Product() {
+	  
+  }
+  
+  @Test
+  public void Product_04_Delete_Product() {
+	  
+  }
+}
